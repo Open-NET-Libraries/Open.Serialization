5140c25b0804324f9979401130a119a1625dbd89
Oren (electricessence) <oren@electrified.net>
2019-10-30 20:34:32 -0700

    Make options 'optional'.  Fixed spelling.

--- Open.Serialization.Json/IJsonSerializationOptions.cs
+++ Open.Serialization.Json/IJsonSerializationOptions.cs
@@ -8,21 +8,21 @@
 		/// <summary>
 		/// If true the serializer will format the JSON properties camelCase.
 		/// </summary>
-		bool CamelCaseProperties { get; }
+		bool? CamelCaseProperties { get; }
 
 		/// <summary>
 		/// If true the serializer will format the JSON map keys camelCase.
 		/// </summary>
-		bool CamelCaseKeys { get; }
+		bool? CamelCaseKeys { get; }
 
 		/// <summary>
 		/// If true the serializer will omit map entries with null values.
 		/// </summary>
-		bool OmitNull { get; }
+		bool? OmitNull { get; }
 
 		/// <summary>
 		/// If true the serializer will format the JSON multi-line indented.
 		/// </summary>
-		bool Indent { get; }
+		bool? Indent { get; }
 	}
 }
---
+++ Open.Serialization.Json/JsonSerializationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Open.Serialization.Json
+{
+	/// <inheritdoc />
+	public class JsonSerializationOptions : IJsonSerializationOptions
+	{
+		public bool? CamelCaseProperties { get; set; }
+
+		public bool? CamelCaseKeys { get; set; }
+
+		public bool? OmitNull { get; set; }
+
+		public bool? Indent { get; set; }
+	}
+}
