436db13c1a86e33bd947641e72da3281a6205b8a
electricessence <5899455+electricessence@users.noreply.github.com>
2020-01-09 12:44:04 -0800

    More specific demonstrated tests.

--- Open.Serialization.Tests/DefaultImplementationTests.cs
+++ Open.Serialization.Tests/DefaultImplementationTests.cs
@@ -21,11 +21,25 @@ namespace Open.Serialization.Tests
 			}
 		}
 
+		class C : B, IDeserializeObject
+		{
+
+		}
+
 		[Fact]
 		public static void DefaultImplementation()
 		{
-			IDeserializeObject a = new A();
-			Assert.Equal(1, a.Deserialize<int>("0"));
+			IDeserializeObject a1 = new A();
+			Assert.Equal(1, a1.Deserialize<int>("0"));
+
+			var b1 = new B();
+			Assert.Equal(0, b1.Deserialize<int>("0"));
+
+			IDeserializeObject b2 = new B();
+			Assert.Equal(1, b2.Deserialize<int>("0"));
+
+			var c1 = new C();
+			Assert.Equal(0, c1.Deserialize<int>("0"));
 		}
 	}
 }
